d8ac0e7b5b2edf6c7c494049e662e7bdaef8084e
Elshan-Dunyamaliyev <elshan.dunyamaliyev@aist.group>
2025-05-01 14:51:50 +0400

    add cart functionality

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,15 +1,39 @@
 const Cart = require("../models/cart");
+const Product = require("../models/product");
+const fs = require("fs");
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    path: "/cart",
+  fs.readFile("cart.json", "utf-8", (err, data) => {
+    const parsedData = JSON.parse(data);
+    const { products, totalPrice } = parsedData;
+    const productsId = products.map((product) => product.id);
+    const cartInProducts = [];
+    let length = 0;
+    if (productsId.length == 0) {
+      return res.render("shop/cart", {
+        path: "/cart",
+        products: [],
+        totalPrice: 0,
+      });
+    }
+    productsId.forEach((id) => {
+      Product.findById(id, (prod) => {
+        cartInProducts.push(prod);
+        length++;
+        if (length == productsId.length) {
+          res.render("shop/cart", {
+            path: "/cart",
+            products: cartInProducts,
+            totalPrice,
+          });
+        }
+      });
+    });
   });
 };
 
 exports.postCart = (req, res, next) => {
   const { productId, productPrice } = req.body;
   Cart.addProduct(productId, +productPrice);
-  res.render("shop/cart", {
-    path: "/cart",
-  });
+  res.redirect("/");
 };
--- models/cart.js
+++ models/cart.js
@@ -4,25 +4,36 @@ module.exports = class Cart {
   static addProduct(id, price) {
     fs.readFile("cart.json", "utf-8", (err, data) => {
       let cart = { products: [], totalPrice: 0 };
-      if (!err) {
-        cart = JSON.parse(data);
+      if (!err && data) {
+        try {
+          cart = JSON.parse(data);
+        } catch (e) {
+          console.error("Failed to parse cart.json:", e);
+        }
       }
+
       const existingProductIndex = cart.products.findIndex(
         (prod) => prod.id === id
       );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
+
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
-        updatedProduct.qty += existingProduct.qty;
-        cart.products = [...cart.products];
+        updatedProduct.qty += 1;
         cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id, qty: 1 };
-        cart.products = [...cart.products, updatedProduct];
+        cart.products.push(updatedProduct);
       }
-      cart.totalPrice += price;
-      fs.writeFile("cart.json", JSON.stringify(cart), (err) => {});
+
+      cart.totalPrice += +price;
+
+      fs.writeFile("cart.json", JSON.stringify(cart), (err) => {
+        if (err) {
+          console.error("Failed to write cart.json:", err);
+        }
+      });
     });
   }
 };
